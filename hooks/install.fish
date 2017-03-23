6322344e360e9cd379d77e8a9a69a3acf10ea662
Chris Dosé <chris.dose@gmail.com>
2017-03-22 23:05:15 -0700

    Clarify installation message.

--- hooks/install.fish
+++ hooks/install.fish
@@ -1,7 +1,7 @@
 if test -d ~/.asdf
-  echo "using existing asdf in ~./asdf"
+  echo "using existing asdf installation in ~./asdf"
 else
-  echo "installing asdf into ~./asdf..."
+  echo "installing asdf v0.2.1 into ~./asdf..."
 
   git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.2.1
 
