1233a4303cb0b2645b97108ca8256cab4849ebd4
Chris Dosé <chris.dose@gmail.com>
2017-03-22 23:03:00 -0700

    Clarify uninstall message.

--- hooks/uninstall.fish
+++ hooks/uninstall.fish
@@ -1,3 +1,4 @@
-echo "asdf itself was NOT removed. If you'd like to also completely remove asdf and all installed tools/version:"
+echo "asdf itself will NOT be removed. If you'd like to also completely remove asdf and all installed tools/versions:"
 echo ""
-echo "rm -rf ~/.asdf"
+echo "  $ rm -rf ~/.asdf"
+echo ""
