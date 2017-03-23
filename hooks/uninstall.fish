71dce613078db5f0b6f0dcb01e32e2c696e15171
Chris Dosé <chris.dose@gmail.com>
2017-03-22 23:06:17 -0700

    Fix unescaped $ in uninstall message.

--- hooks/uninstall.fish
+++ hooks/uninstall.fish
@@ -1,4 +1,4 @@
 echo "asdf itself will NOT be removed. If you'd like to also completely remove asdf and all installed tools/versions:"
 echo ""
-echo "  $ rm -rf ~/.asdf"
+echo "  \$ rm -rf ~/.asdf"
 echo ""
