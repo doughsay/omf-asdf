1da927fb3ce6b8161a8eed6600a71795f3dc44fa
Chris Dosé <chris.dose@gmail.com>
2018-10-02 19:21:19 -0700

    update asdf version

--- hooks/install.fish
+++ hooks/install.fish
@@ -1,9 +1,9 @@
 if test -d ~/.asdf
   echo "using existing asdf installation in ~./asdf"
 else
-  echo "installing asdf v0.2.1 into ~./asdf..."
+  echo "installing asdf v0.5.1 into ~./asdf..."
 
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.2.1
+  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.1
 
   echo "done!"
 end
